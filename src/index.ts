220bcd61fec21cf706f8b1d4f3dc0d6c3e6e1f1b
Adedapo Adeyemi <adedapo025@gmail.com>
2025-05-11 17:57:36 +0100

    set up server to get data from API

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express"
+import dotenv from "dotenv"
+import axios from "axios"
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json())
+
+interface UrlValidator {
+    location: string,
+    date1 ?: Date,
+    date2 ?: Date
+}
+
+const generateUrl = (obj: UrlValidator) => `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${obj.location}/${obj?.date1}/${obj?.date2}?key=${process.env.API_KEY}`;
+
+const fetchWeatherInfo = async (obj: UrlValidator) => {
+    const url = generateUrl(obj);
+    const response = await axios.get(url);
+    return response
+}
+
+app.get('/get-weather', async (req: Request, res: Response) => {
+    const query = req.query;
+    if (!query.location){
+        throw new Error("")
+    }
+    await fetchWeatherInfo(query);
+
+})
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+})
+
---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+type AsyncHandlerType = (
+    req: Request,
+    res: Response,
+    next?: NextFunction
+) => Promise<any>
+
+export const asyncHandler = (fn: AsyncHandlerType) => 
+    (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next).catch(next))
+    };
+
+    
